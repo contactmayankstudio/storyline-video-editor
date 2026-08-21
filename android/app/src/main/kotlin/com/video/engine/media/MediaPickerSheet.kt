501b0f9f9886f061ac926301c02eb9b2bdc116df
am <sarojshahu12@gmail.com>
2026-08-22 02:50:59 +0530

    fix(media): clean import statements in MediaPickerSheet.kt

--- android/app/src/main/kotlin/com/video/engine/media/MediaPickerSheet.kt
+++ android/app/src/main/kotlin/com/video/engine/media/MediaPickerSheet.kt
@@ -17,7 +17,11 @@ import android.util.Size
 import android.view.Gravity
 import android.view.View
 import android.view.ViewGroup
-import android.widget.*
+import android.widget.Button
+import android.widget.FrameLayout
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
